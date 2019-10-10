86efb1b705983eee6cdc93a5e08565705618faf7
evanfung <evanfungv@gmail.com>
2019-10-10 22:08:37 +0800

    initial commit

---
+++ src/main/java/hk/edu/hkbu/comp/search_engine/MyController.java
@@ -0,0 +1,15 @@
+package hk.edu.hkbu.comp.search_engine;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @GetMapping("**")
+    @ResponseBody
+    String load(HttpServletRequest request) {
+        return "Hello World";
+    }
+}
+
