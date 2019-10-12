65aef17ceb3657c04daf5298bc19450c1724b5ff
badaeib <18200842@life.hkbu.edu.hk>
2019-10-12 15:46:37 +0800

    test

---
+++ src/main/java/hk/edu/hkbu/comp/search_engine/Test.java
@@ -0,0 +1,24 @@
+package hk.edu.hkbu.comp.search_engine;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("=D");
+    }
+
+//    static String[] getWords(String[] webpage)
+//    {
+//
+//    }
+//
+//    static String[] getPhrase(String[] webpage)
+//    {
+//
+//    }
+//
+//    static String[] getURLs(String[] webpage)
+//    {
+//
+//    }
+}
