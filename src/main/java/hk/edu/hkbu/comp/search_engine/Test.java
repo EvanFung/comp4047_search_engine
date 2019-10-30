322525027a507eb4a5b69db02b7ea83841098d08
evanfung <evanfungv@gmail.com>
2019-10-30 17:19:26 +0800

    html added

--- src/main/java/hk/edu/hkbu/comp/search_engine/Test.java
+++ src/main/java/hk/edu/hkbu/comp/search_engine/Test.java
@@ -5,10 +5,7 @@ import hk.edu.hkbu.comp.search_engine.crawler.Crawler;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-//        Crawler crawler = new Crawler("http://www.comp.hkbu.edu.hk/", 10,100);
-//        crawler.crawling();
-        String s = "APPLE PEAR";
-        String s2 =  s.replaceAll(" ", "AND");
-        System.out.println(s2);
+        Crawler crawler = new Crawler("http://www.comp.hkbu.edu.hk/", 10,100);
+        crawler.crawling();
     }
 }
