fcaae1c30b4aeb0ddc546bd898dc121d969efd11
evanfung <evanfungv@gmail.com>
2019-10-31 21:43:03 +0800

    Merge branch 'master' of https://github.com/EvanFung/comp4047_search_engine
    # Conflicts:
    #	src/main/java/hk/edu/hkbu/comp/search_engine/SearchController.java

--- src/main/java/hk/edu/hkbu/comp/search_engine/utils/Utils.java
+++ src/main/java/hk/edu/hkbu/comp/search_engine/utils/Utils.java
@@ -1,5 +1,11 @@
 package hk.edu.hkbu.comp.search_engine.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +25,36 @@ public class Utils {
 
         return uniqueWords;
     }
+
+
+    public static String loadWebConent(String urlStr) {
+        URL url;
+        InputStream is = null;
+        BufferedReader br;
+        String line;
+        String result = "";
+        try {
+            url = new URL(urlStr);
+            is = url.openStream();  // throws an IOException
+            br = new BufferedReader(new InputStreamReader(is));
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+        } catch (MalformedURLException mue) {
+            mue.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+            } catch (IOException ioe) {
+                // nothing to see here
+            }
+        }
+
+        return result;
+    }
     //        SensitiveFilterService filter = new SensitiveFilterService(SensitiveType.url);
 //        String txt = "http://www.comp.hkbu.edu.hk/v1/?page=people";
 //        //如果需要过滤则用“”替换
@@ -28,4 +64,20 @@ public class Utils {
 //        System.out.println(a);
 //        System.out.println("替换前的文字为：" + txt);
 //        System.out.println("替换后的文字为：" + hou);
+
+    public static String toUsefulText(String str) {
+        str = str.replaceAll("\\<(img)[^>]*>|<\\/(img)>", "");
+        str = str.replaceAll("\\<(table|tbody|tr|td|th|)[^>]*>|<\\/(table|tbody|tr|td|th|)>", "");
+        str = str.replaceAll("\\<(div|blockquote|fieldset|legend)[^>]*>|<\\/(div|blockquote|fieldset|legend)>", "");
+        str = str.replaceAll("\\<(font|i|u|h[1-9]|s)[^>]*>|<\\/(font|i|u|h[1-9]|s)>", "");
+        str = str.replaceAll("\\<(style|strong)[^>]*>|<\\/(style|strong)>", "");
+        str = str.replaceAll("\\<a[^>]*>|<\\/a>", "");
+        str = str.replaceAll("\\<(meta|iframe|frame|span|tbody|layer)[^>]*>|<\\/(iframe|frame|meta|span|tbody|layer)>",
+                "");
+        str = str.replaceAll("\\<br[^>]*", "");
+        str = str.replaceAll("<script[\\s\\s]+</script *>", "");
+        str = str.replaceAll("\\s", "");
+        return str;
+    }
+
 }
