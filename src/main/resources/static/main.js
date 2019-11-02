7e63ae900a8e955b8591a077654d6456cca1988e
evanfung <evanfungv@gmail.com>
2019-11-03 02:58:42 +0800

    highlight keyword

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,47 @@
+// var query = document.getElementById('queryStr');
+// var words = query.split(' ');
+//
+//
+// if(words != null) {
+//     for(var i = 0;i < words.length; i++) {
+//         highlight(words[i]);
+//     }
+// }
+//
+//
+// function highlight(text) {
+//     var partialContent = document.getElementsByClassName("partial-content");
+//     for(var i = 0;i < partialContent.length; i++) {
+//         var innerHTML = partialContent[i].innerHTML;
+//         var index = innerHTML.indexOf(text);
+//         if (index >= 0) {
+//             innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+//             partialContent[i].innerHTML = innerHTML;
+//         }
+//     }
+// }
+
+(function () {
+    var query = document.getElementById('queryStr');
+    if(query != null) {
+        var words = query.value.split(' ');
+    }
+    if (words != null && query != null) {
+        for (var i = 0; i < words.length; i++) {
+            highlight(words[i]);
+        }
+    }
+
+
+    function highlight(text) {
+        var partialContent = document.getElementsByClassName("partial-content");
+        for (var i = 0; i < partialContent.length; i++) {
+            var innerHTML = partialContent[i].innerHTML;
+            var index = innerHTML.indexOf(text);
+            if (index >= 0) {
+                innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+                partialContent[i].innerHTML = innerHTML;
+            }
+        }
+    }
+})()
